a770b2e979748cd584cddd114c0c785f23f0c1cf
Kudzeri <kudzeri@gmail.com>
2024-08-11 14:07:12 +0500

    add role column to users table

--- database/migrations/2024_08_11_090405_add_role_to_users_table.php
+++ database/migrations/2024_08_11_090405_add_role_to_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Couchbase\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->after('name');
+            $table->string('role')->after('name')->default('user');
         });
     }
 
