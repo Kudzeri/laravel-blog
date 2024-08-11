10cd2c3864359e96f17323268d42f612388ea54a
Kudzeri <kudzeri@gmail.com>
2024-08-11 14:54:47 +0500

    admin page

---
+++ resources/views/components/admin-nav.blade.php
@@ -0,0 +1,11 @@
+<nav class="app-header navbar navbar-expand bg-body"> <!--begin::Container-->
+    <div class="container-fluid"> <!--begin::Start Navbar Links-->
+        <ul class="navbar-nav">
+            <li class="nav-item"> <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button"> <i class="bi bi-list"></i> </a> </li>
+        </ul>
+        <ul class="navbar-nav ms-auto"> <!--begin::Navbar Search-->
+            <li class="nav-item dropdown user-menu"> <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"> <img src="../../dist/assets/img/user2-160x160.jpg" class="user-image rounded-circle shadow" alt="User Image"> <span class="d-none d-md-inline">Alexander Pierce</span> </a>
+            </li> <!--end::User Menu Dropdown-->
+        </ul> <!--end::End Navbar Links-->
+    </div> <!--end::Container-->
+</nav> <!--end::Header--> <!--begin::Sidebar-->
