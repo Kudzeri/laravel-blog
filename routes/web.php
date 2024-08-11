ce8f8617e0c2efdd0786457eaa8b4fd506e60279
Kudzeri <kudzeri@gmail.com>
2024-08-11 14:27:26 +0500

    add admin route and page

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\AdminController;
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/', [PostController::class, 'index'])->name('index');
-Route::resource('posts', PostController::class);
+
+Route::resource('posts', PostController::class)->only(['index', 'show']);
+Route::resource('categories', CategoryController::class)->only(['index', 'show']);
+Route::resource('tags', TagController::class)->only(['index', 'show']);
+
+Route::group(['middleware' => ['auth'], 'prefix' => 'admin'], function () {
+    Route::get('/', [AdminController::class, "index"])->name('admin.index');
+});
