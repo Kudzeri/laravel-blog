beb8cdc15bfb6bf79714b66b5305cbc078537c08
Kudzeri <kudzeri@gmail.com>
2024-08-12 13:15:41 +0500

    add Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,8 @@ class Category extends Model
     protected $table = 'categories';
     protected $guarded = ['id'];
     protected $fillable = ['name'];
+
+    function posts() {
+        return $this->hasMany(Post::class);
+    }
 }
