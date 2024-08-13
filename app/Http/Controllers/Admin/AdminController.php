551601d187c900c6dd9eeab146404c3daf4d9293
Kudzeri <kudzeri@gmail.com>
2024-08-13 10:18:13 +0500

    update index.blade.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-   public function index ()
-   {
-       return view('admin.index');
-   }
+    public function index()
+    {
+        return view('admin.index');
+    }
 }
